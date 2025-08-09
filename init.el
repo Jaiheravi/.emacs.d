49ac9ec477a72408436a2a8a40cff698eed5d0fc
Jaime Hernandez <contact@jaime.one>
2025-08-08 19:17:42 -0600

    Add initial setup for Scheme

--- init.el
+++ init.el
@@ -1,18 +1,8 @@
-;; ==================================================
-;; Priority settings
-
 ;; Prevent package.el loading packages prior to their init-file loading
 ;; This is a recommendation from the documentation of straight.el
 (setq package-enable-at-startup nil)
 
-;; Don't show a startup creen
-(setq inhibit-startup-screen t)
-
-;; Avoid having backup and autosave files everywhere
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t)))
-
-;; ==================================================
+;; ================================================================================
 ;; Packages
 
 ;; Set up the "Straight" package manager
@@ -50,7 +40,8 @@
 ;; Simultaneous editing of occurences
 (use-package iedit)
 
-;; ========== Start OCaml Settings  ==========
+;; --------------------------------------------------
+;; OCaml Setup
 
 (use-package tuareg
   :mode
@@ -72,10 +63,84 @@
   :config
   (flycheck-ocaml-setup))
 
-;; ========== End OCaml Settings ==========
+;; --------------------------------------------------
+;; Scheme setup
+
+(use-package paredit
+  :hook (emacs-lisp-mode       . paredit-mode)
+  :hook (eval-expression-minibuffer-setup . paredit-mode)
+  :hook (ielm-mode             . paredit-mode)
+  :hook (lisp-mode             . paredit-mode)
+  :hook (lisp-interaction-mode . paredit-mode)
+  :hook (scheme-mode           . paredit-mode))
+
+
+;; ================================================================================
+;; Custom functions
+
+;; Duplicate line
+(defun duplicate-line ()
+  "Duplicate current line."
+  (interactive)
+  (let ((line (buffer-substring (line-beginning-position) (line-end-position))))
+    (end-of-line)
+    (newline)
+    (insert line)))
+
+(defun custom-beginning-of-line ()
+ "Toggle between the first non-whitespace character and the beginning of the line"
+ (interactive)
+ (let ((orig-point (point)))
+   (back-to-indentation)
+   (when (= orig-point (point))
+     (move-beginning-of-line 1))))
+
+(defun copy-to-clipboard ()
+  "Copies selection to x-clipboard."
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+	(message "Yanked region to x-clipboard!")
+	(call-interactively 'clipboard-kill-ring-save)
+	)
+    (if (region-active-p)
+	(progn
+	  (shell-command-on-region (region-beginning) (region-end) "pbcopy")
+	  (message "Yanked region to clipboard!")
+	  (deactivate-mark))
+      (message "No region active; can't yank to clipboard!")))
+  )
+
+(defun paste-from-clipboard ()
+  "Pastes from x-clipboard."
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+	(clipboard-yank)
+	(message "graphics active")
+	)
+    (insert (shell-command-to-string "pbpaste"))
+    )
+  )
 
 ;; =============================================================================
 ;; General settings
+
+;; Don't show a startup creen
+(setq inhibit-startup-screen t)
+
+;; Avoid having backup and autosave files everywhere
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/" t)))
+
+;; Make sure there is always a new line at the end of the file
+(setq require-final-newline t)
+
+;; Remove trailing whitespace when saving a file
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Remove the dash separator in the mode line
+(setq-default mode-line-end-spaces "")
 
 ;; Hide menu bar
 (menu-bar-mode -1)
@@ -108,68 +173,11 @@
 (set-face-background 'iedit-occurrence "#E1ECEB")
 (set-face-background 'region "#FAEEC6")
 
-;; Make sure there is always a new line at the end of the file
-(setq require-final-newline t)
-
-;; Remove trailing whitespace when saving a file
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; Remove the dash separator in the mode line
-(setq-default mode-line-end-spaces "")
-
-;; Duplicate line
-(defun duplicate-line ()
-  "Duplicate current line."
-  (interactive)
-  (let ((line (buffer-substring (line-beginning-position) (line-end-position))))
-    (end-of-line)
-    (newline)
-    (insert line)))
-
-;; ==================================================
-;; Functions
-
-(defun metaverso-beginning-of-line ()
-  "Toggle between the first non-whitespace character and the beginning of the line"
-  (interactive)
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-(defun copy-to-clipboard ()
-  "Copies selection to x-clipboard."
-  (interactive)
-  (if (display-graphic-p)
-      (progn
-	(message "Yanked region to x-clipboard!")
-	(call-interactively 'clipboard-kill-ring-save)
-	)
-    (if (region-active-p)
-	(progn
-	  (shell-command-on-region (region-beginning) (region-end) "pbcopy")
-	  (message "Yanked region to clipboard!")
-	  (deactivate-mark))
-      (message "No region active; can't yank to clipboard!")))
-  )
-
-(defun paste-from-clipboard ()
-  "Pastes from x-clipboard."
-  (interactive)
-  (if (display-graphic-p)
-      (progn
-	(clipboard-yank)
-	(message "graphics active")
-	)
-    (insert (shell-command-to-string "pbpaste"))
-    )
-  )
-
 ;; ==================================================
 ;; Keybindings
 
 ;; Redefine core keybindings
-(define-key global-map (kbd "C-a") '("Go to beginning of line" . metaverso-beginning-of-line))
+(define-key global-map (kbd "C-a") '("Go to beginning of line" . custom-beginning-of-line))
 
 ;; Clipboard keybindings
 (define-key global-map (kbd "C-c c") '("Copy to clipboard" . copy-to-clipboard))
