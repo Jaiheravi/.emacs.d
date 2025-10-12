1038c92eb6535d02d4145abcf9f56f76f2d3d758
Jaime Hernandez <contact@jaime.one>
2025-10-12 12:24:12 -0600

    Add treesiter and lsp for web dev

--- init.el
+++ init.el
@@ -171,47 +171,32 @@
 ;; Tree-sitter Language Grammars
 ;; Run M-x treesit-install-language-grammar to install the grammars listed here
 (setq treesit-language-source-alist
-      '(
-
-        (css . ("https://github.com/tree-sitter/tree-sitter-css"))
-
-
+      '((css . ("https://github.com/tree-sitter/tree-sitter-css"))
         (html . ("https://github.com/tree-sitter/tree-sitter-html"))
         (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
         (json . ("https://github.com/tree-sitter/tree-sitter-json"))
-
-
-
         (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
-
-
-
-        (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
-        ))
-
-(defun trim-leading-whitespace ()
-  "Delete all leading whitespace from lines that start with it in the current buffer."
-  (interactive)
-  (goto-char (point-min))
-  (while (re-search-forward "^[ \t]+" nil t)
-    (replace-match "" nil nil)))
+        (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))))
 
 ;; Replace modes with Tree-sitter modes
 (setq major-mode-remap-alist
-      '((typescript-mode . typescript-ts-mode)
-      (css-mode . css-ts-mode)
+      '((css-mode . css-ts-mode)
       (html-mode . html-ts-mode)
-      (javascript-mode . javascript-ts-mode)))
+      (js-mode . javascript-ts-mode)
+      (json-mode . json-ts-mode)
+      (typescript-mode . typescript-ts-mode)))
 
-;; Associate file type with typescript-ts-mode
-(add-to-list 'auto-mode-alist '(
-				("\\.ts\\'" . typescript-ts-mode)
-				("\\.css\\'" . css-ts-mode)
-				("\\.html\\'" . html-ts-mode)
-				("\\.js\\'" . javascript-ts-mode)
-				("\\.json\\'" . json-ts-mode)))
+;; LSP stuff
+;; We mainly want this for Typescript
+(use-package lsp-mode
+  :hook ((typescript-ts-mode . lsp-deferred)
+	 (tsx-ts-mode . lsp-deferred)
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :config
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq warning-suppress-types '((lsp-mode) (lsp-mode))))
 
-;; (add-to-list 'auto-mode-alist '(("\\.js\\'" . javascript-ts-mode)))
 
 ;; Display the undo tree
 (use-package vundo
@@ -396,22 +381,7 @@
 ;; Smalltalk
 
 ;; --------------------------------------------------
-;; Typescript
-
-
-;; LSP stuff
-(use-package lsp-mode
-  :hook ((typescript-ts-mode . lsp-deferred)
-	 (tsx-ts-mode . lsp-deferred)
-	 (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp
-  :config
-  (setq lsp-headerline-breadcrumb-enable nil))
-
-
-
-(setq major-mode-remap-alist
-      '((typescript-mode . typescript-ts-mode)))
+;; Typescript / Javascript
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
