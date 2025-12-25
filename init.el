9ec4dcedd8f20bb43b725d601d43f036c59b5444
Jaime Hernandez <contact@jaime.one>
2025-12-25 15:47:58 -0600

    Remove Emacs own editing

--- init.el
+++ init.el
@@ -343,17 +343,3 @@
 (set-face-attribute 'font-lock-punctuation-face nil :foreground rose-subtle)
 (set-face-attribute 'font-lock-bracket-face nil :foreground rose-muted)
 (set-face-attribute 'font-lock-delimiter-face nil :foreground rose-muted)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(denote flycheck geiser-chez iedit latex-preview-pane lsp-mode
-            marginalia olivetti spell-fu vertico yasnippet)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
